e03d054b20d6d9272d04b9b055cea27ef01de65b
IvanKulakov <84179036+IvanKulakov@users.noreply.github.com>
2024-11-05 01:02:30 +0200

    work

--- slider.js
+++ slider.js
@@ -2,7 +2,7 @@ const slider = document.querySelector('.swiper');
 const sl = new Swiper(slider, {
     // Optional parameters
     direction: 'horizontal',
-    slidesPerView: 6,
+    slidesPerView: 5,
     loop: true,
     speed: 2000,
     autoplay: {
